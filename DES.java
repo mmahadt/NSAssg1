d0df09499c16ff620b6b5e08e307c461690c1af6
Muhammad Mahad Tariq <14besemmahad@seecs.edu.pk>
2017-10-18 21:49:25 +0500

    Deleted 3DES code

--- DES.java
+++ DES.java
@@ -313,86 +313,6 @@ public class DES {
 		return tmp;
 	}
 	
-/*	public static byte[] TripleDES_Encrypt(byte[] data,byte[][] keys)
-	{
-		int lenght=0;
-		byte[] padding = new byte[1];
-		int i;
-
-		lenght = 8 - data.length % 8;
-		padding = new byte[lenght];
-		padding[0] = (byte) 0x80;
-	
-		for (i = 1; i < lenght; i++)
-			padding[i] = 0;
-
-
-		byte[] tmp = new byte[data.length + lenght];
-		byte[] bloc = new byte[8];
-		
-
-		K = generateSubKeys(keys[0]);
-		K1 = generateSubKeys(keys[1]);
-		K2 = generateSubKeys(keys[2]);
-		
-		int count = 0;
-
-		for (i = 0; i < data.length + lenght; i++) {
-			if (i > 0 && i % 8 == 0) {
-				bloc = encrypt64Bloc(bloc,K, false);						
-				bloc = encrypt64Bloc(bloc,K1, true);		
-				bloc = encrypt64Bloc(bloc,K2, false);
-				System.arraycopy(bloc, 0, tmp, i - 8, bloc.length);
-			}
-			if (i < data.length)
-				bloc[i % 8] = data[i];
-			else {														
-				bloc[i % 8] = padding[count % 8];
-				count++;
-			}
-		}
-		if(bloc.length == 8){
-			bloc = encrypt64Bloc(bloc,K, false);
-			bloc = encrypt64Bloc(bloc,K1, true);
-			bloc = encrypt64Bloc(bloc,K2, false);
-			System.arraycopy(bloc, 0, tmp, i - 8, bloc.length);
-		}
-		return tmp;
-	}*/
-	
-	
-	/*public static byte[] TripleDES_Decrypt(byte[] data,byte[][] keys)
-	{
-		int i;
-		byte[] tmp = new byte[data.length];
-		byte[] bloc = new byte[8];
-
-		K = generateSubKeys(keys[0]);
-		K1 = generateSubKeys(keys[1]);
-		K2 = generateSubKeys(keys[2]);
-
-		for (i = 0; i < data.length; i++) {
-			if (i > 0 && i % 8 == 0) {
-				bloc = encrypt64Bloc(bloc,K2, true);
-				bloc = encrypt64Bloc(bloc,K1, false);
-				bloc = encrypt64Bloc(bloc,K, true);
-				System.arraycopy(bloc, 0, tmp, i - 8, bloc.length);
-			}
-			if (i < data.length)
-				bloc[i % 8] = data[i];
-		}
-		bloc = encrypt64Bloc(bloc,K2, true);
-		bloc = encrypt64Bloc(bloc,K1, false);
-		bloc = encrypt64Bloc(bloc,K, true);
-		System.arraycopy(bloc, 0, tmp, i - 8, bloc.length);
-
-		
-		tmp = deletePadding(tmp);
-
-		return tmp;
-	}*/
-
-	
 	public static byte[] decrypt(byte[] data, byte[] key) {
 		int i;
 		byte[] tmp = new byte[data.length];
@@ -439,6 +359,7 @@ public class DES {
             byteToBits(original[i]);
         }
         System.out.println();
+		return 1;
     }
 
     private static void printBytes(byte[] data) {
