86666d657be94850b2cb9ff78787df692174302f
Muhammad Mahad Tariq <14besemmahad@seecs.edu.pk>
2017-10-20 10:41:30 +0500

    Alhamdulillah, bugs fixed

--- DES.java
+++ DES.java
@@ -2,42 +2,42 @@
 
 public class DES {
     // initial permuation table
-    private static int[] IP = { 58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36,
+    private int[] IP = { 58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36,
             28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32,
             24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19,
             11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7 };
     // inverse initial permutation
-    private static int[] invIP = { 40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47,
+    private int[] invIP = { 40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47,
             15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13,
             53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51,
             19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17,
             57, 25 };
     // Permutation P (in f(Feistel) function)
-    private static int[] P = { 16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5,
+    private int[] P = { 16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5,
             18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4,
             25
 
     };
     // initial key permutation 64 => 56 biti
-    private static int[] PC1 = { 57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34,
+    private int[] PC1 = { 57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34,
             26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63,
             55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53,
             45, 37, 29, 21, 13, 5, 28, 20, 12, 4 };
     // key permutation at round i 56 => 48
-    private static int[] PC2 = { 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
+    private int[] PC2 = { 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
             23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55,
             30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29,
             32 };
     // key shift for each round
-    private static int[] keyShift = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2,
+    private int[] keyShift = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2,
             2, 1 };
     // expansion permutation from function f
-    private static int[] expandTbl = { 32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8,
+    private int[] expandTbl = { 32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8,
             9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21,
             20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32,
             1 };
     // substitution boxes
-    private static int[][][] sboxes = {
+    private int[][][] sboxes = {
             { 		{ 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 },
                     { 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 },
                     { 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 },
@@ -87,11 +87,11 @@ public class DES {
 	/*private static byte[][] K1;
 	private static byte[][] K2;*/
 
-    private static int idx = 0;
+    private int idx = 0;
 
-    private static String[] intermediates = new String[67];
+    private String[] intermediates = new String[67];
 
-    private static void setBit(byte[] data, int pos, int val) {
+    private void setBit(byte[] data, int pos, int val) {
         int posByte = pos / 8;
         int posBit = pos % 8;
         byte tmpB = data[posByte];
@@ -100,7 +100,7 @@ public class DES {
         data[posByte] = newByte;
     }
 
-    private static int extractBit(byte[] data, int pos) {
+    private int extractBit(byte[] data, int pos) {
         int posByte = pos / 8;
         int posBit = pos % 8;
         byte tmpB = data[posByte];
@@ -108,7 +108,7 @@ public class DES {
         return bit;
     }
 
-    private static byte[] rotLeft(byte[] input, int len, int pas) {
+    private byte[] rotLeft(byte[] input, int len, int pas) {
         int nrBytes = (len - 1) / 8 + 1;
         byte[] out = new byte[nrBytes];
         for (int i = 0; i < len; i++) {
@@ -118,7 +118,7 @@ public class DES {
         return out;
     }
 
-    private static byte[] extractBits(byte[] input, int pos, int n) {
+    private byte[] extractBits(byte[] input, int pos, int n) {
         int numOfBytes = (n - 1) / 8 + 1;
         byte[] out = new byte[numOfBytes];
         for (int i = 0; i < n; i++) {
@@ -129,7 +129,7 @@ public class DES {
 
     }
 
-    private static byte[] permutFunc(byte[] input, int[] table) {
+    private byte[] permutFunc(byte[] input, int[] table) {
         int nrBytes = (table.length - 1) / 8 + 1;
         byte[] out = new byte[nrBytes];
         for (int i = 0; i < table.length; i++) {
@@ -140,7 +140,7 @@ public class DES {
         return out;
     }
 
-    private static byte[] xor_func(byte[] a, byte[] b) {
+    private byte[] xor_func(byte[] a, byte[] b) {
         byte[] out = new byte[a.length];
         for (int i = 0; i < a.length; i++) {
             out[i] = (byte) (a[i] ^ b[i]);
@@ -149,7 +149,7 @@ public class DES {
 
     }
 
-    private static byte[] encrypt64Bloc(byte[] bloc,byte[][] subkeys, boolean isDecrypt) {
+    private byte[] encrypt64Bloc(byte[] bloc,byte[][] subkeys, boolean isDecrypt) {
         byte[] tmp = new byte[bloc.length];
         byte[] R = new byte[bloc.length / 2];
         byte[] L = new byte[bloc.length / 2];
@@ -174,11 +174,11 @@ public class DES {
         tmp = concatBits(R, IP.length / 2, L, IP.length / 2);
         tmp = permutFunc(tmp, invIP);
         intermediates[idx]=byteArrayToBits(tmp);++idx;
-        idx=0;
+
         return tmp;
     }
 
-    private static byte[] f_func(byte[] R, byte[] K) {
+    private byte[] f_func(byte[] R, byte[] K) {
         byte[] tmp;
         tmp = permutFunc(R, expandTbl);
         intermediates[idx]=byteArrayToBits(tmp);++idx;
@@ -191,7 +191,7 @@ public class DES {
         return tmp;
     }
 
-    private static byte[] s_func(byte[] in) {
+    private byte[] s_func(byte[] in) {
         in = separateBytes(in, 6);
         byte[] out = new byte[in.length / 2];
         int halfByte = 0;
@@ -208,7 +208,7 @@ public class DES {
         return out;
     }
 
-    private static byte[] separateBytes(byte[] in, int len) {
+    private byte[] separateBytes(byte[] in, int len) {
         int numOfBytes = (8 * in.length - 1) / len + 1;
         byte[] out = new byte[numOfBytes];
         for (int i = 0; i < numOfBytes; i++) {
@@ -220,7 +220,7 @@ public class DES {
         return out;
     }
 
-    private static byte[] concatBits(byte[] a, int aLen, byte[] b, int bLen) {
+    private byte[] concatBits(byte[] a, int aLen, byte[] b, int bLen) {
         int numOfBytes = (aLen + bLen - 1) / 8 + 1;
         byte[] out = new byte[numOfBytes];
         int j = 0;
@@ -238,7 +238,7 @@ public class DES {
     }
 
 
-    private static byte[] deletePadding(byte[] input) {
+    private byte[] deletePadding(byte[] input) {
         int count = 0;
 
         int i = input.length - 1;
@@ -254,7 +254,7 @@ public class DES {
 
 
 
-    private static byte[][] generateSubKeys(byte[] key) {
+    private byte[][] generateSubKeys(byte[] key) {
         byte[][] tmp = new byte[16][];
         byte[] tmpK = permutFunc(key, PC1);
 
@@ -274,7 +274,7 @@ public class DES {
         return tmp;
     }
 
-    public static byte[] encrypt(byte[] data, byte[] key) {
+    public byte[] encrypt(byte[] data, byte[] key) {
         int lenght=0;
         byte[] padding = new byte[1];
         int i;
@@ -351,7 +351,7 @@ public class DES {
         return intermediates;
     }
 
-    public static byte[] decrypt(byte[] data, byte[] key) {
+    public byte[] decrypt(byte[] data, byte[] key) {
         int i;
         byte[] tmp = new byte[data.length];
         byte[] bloc = new byte[8];
@@ -375,14 +375,14 @@ public class DES {
         return tmp;
     }
 
-    private static String byteToBits(byte b) {
+    private String byteToBits(byte b) {
         StringBuffer buf = new StringBuffer();
         for (int i=0; i<8; i++)
             buf.append((int)(b>>(8-(i+1)) & 0x0001));
         return buf.toString();
     }
 
-    private static String byteArrayToBits(byte[] byteArray) {
+    private String byteArrayToBits(byte[] byteArray) {
         String bitString = "";
 
         for (int i=0; i<byteArray.length; i++) {
@@ -392,7 +392,7 @@ public class DES {
         return bitString;
     }
 
-    private static void printBytes(byte[] data, String name) {
+    private void printBytes(byte[] data, String name) {
         System.out.println("");
         System.out.println(name + ":");
         for (int i=0; i<data.length; i++) {
@@ -401,7 +401,7 @@ public class DES {
         System.out.println();
     }
 
-    private static int compareBitStrings(String original, String encoded) {
+    private int compareBitStrings(String original, String encoded) {
         int count = 0;
         if (original.length() == encoded.length()){
             for (int i = 0; i < original.length()-1; i++) {
@@ -422,7 +422,7 @@ public class DES {
         return count;
     }
 
-    private static void printBytes(byte[] data) {
+    private void printBytes(byte[] data) {
         for (int i=0; i<data.length; i++) {
             System.out.print(byteToBits(data[i])+" ");
         }
@@ -433,21 +433,21 @@ public class DES {
         // TODO Auto-generated method stub
         try
         {
-            String textClar = "DES Alg";
-            String updated = "EES Alg";
+            String textClar = "ABCDEF";
+            String updated = "BBCDEF";
             //DES
-            String k = "5qw8sd4h";
+            String k = "randomchars";
 
             DES d = new DES();
+            DES des = new DES();
 
             String[] original = d.encryptAndReturnIntermediatesArray(textClar.getBytes(), k.getBytes());
-            String[] updatedString = d.encryptAndReturnIntermediatesArray(updated.getBytes(), k.getBytes());
+            String[] updatedString = des.encryptAndReturnIntermediatesArray(updated.getBytes(), k.getBytes());
 
             System.out.println("---------------------------------------------------------------------------------------");
             System.out.println("Plaintext:\t"+textClar);
             System.out.println("Length:\t" + textClar.getBytes().length);
             System.out.print("Original Contents (bin):\t");
-            printBytes(textClar.getBytes());
             System.out.println("---------------------------------------------------------------------------------------");
 
             int counter = 0;
@@ -476,7 +476,7 @@ public class DES {
                     System.out.println("\t\tOutput length:"+original[counter].length());
                     System.out.println("\t\tUpdated Contents (bin):"+original[counter]);
                     System.out.println("\t\tOriginal Contents (bin):"+updatedString[counter]);
-                    System.out.println("\t\tAvalanche Effect:"+compareBitStrings(original[counter],updatedString[counter]));
+                    System.out.println("\t\tAvalanche Effect:"+d.compareBitStrings(original[counter], updatedString[counter]));
                     ++counter;
                 }
             }
@@ -484,10 +484,10 @@ public class DES {
             System.out.println("---------------------------------------------------------------------------------------");
             System.out.println("Inverse Initial Permutation (invIP):");
             System.out.println("\t\tOutput length:"+original[counter].length());
-            System.out.println("\t\tUpdated Contents (bin):"+original[counter]);
-            System.out.println("\t\tOriginal Contents (bin):"+updatedString[counter]);
-            System.out.println("\t\tAvalanche Effect:"+compareBitStrings(original[counter],updatedString[counter]));
-            System.out.println("\t\tAvalanche Effect:"+compareBitStrings("1101","0011"));
+            System.out.println("\t\tUpdated Contents (bin):"+updatedString[counter]);
+            System.out.println("\t\tOriginal Contents (bin):"+original[counter]);
+            System.out.println("\t\tAvalanche Effect:" + d.compareBitStrings(original[counter], updatedString[counter]));
+//            System.out.println("\t\tAvalanche Effect:"+d.compareBitStrings("1101","00111"));
             //TODO: compare bit string throws error on different length string
 
             //TODO: correct static context of intermediate
